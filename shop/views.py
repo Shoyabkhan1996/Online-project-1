7adef68ea069e3157901f4eb54ac75d9a86470df
Shoyabkhan1996 <89289711+Shoyabkhan1996@users.noreply.github.com>
2021-11-15 09:22:27 +0545

    Add files via upload

---
+++ shop/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,redirect,reverse
+from .models import *
+# from .forms import *
+from django.http import HttpResponseRedirect,HttpResponse
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+# Create your views here.
+def Index(request):
+    product_objects=Products.objects.all()
+    # search code
+    item_name= request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects=product_objects.filter(title__icontains=item_name)
+    
+    # paginator code
+    paginator=Paginator(product_objects,4)
+    page=request.GET.get('page')
+    product_objects=paginator.get_page(page)
+
+
+    
+    
+    return render(request,'ecom/index.html',{'product_objects':product_objects})
+
+
+def detail(request,id):
+    product_object=Product.objects.get(id=id)
+    return render(request,"shop/detail.html",{'product_object':product_object})
+
